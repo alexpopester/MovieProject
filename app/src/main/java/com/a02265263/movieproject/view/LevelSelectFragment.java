c1df570167d4bbe7fa16b7b90d38dcb658fa7435
MatthewRau99 <matthewrau99@gmail.com>
2022-04-30 16:50:53 -0600

    finished most of the UI for start and level screens

--- app/src/main/java/com/a02265263/movieproject/view/LevelSelectFragment.java
+++ app/src/main/java/com/a02265263/movieproject/view/LevelSelectFragment.java
@@ -11,6 +11,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -24,6 +25,7 @@ public class LevelSelectFragment extends Fragment {
     private int level = 0;
     private ConstraintLayout levelDetails;
     private View view;
+    private boolean isUp;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,61 +35,50 @@ public class LevelSelectFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_level_select, container,false);
 
+        isUp = false;
         levelDetails = view.findViewById(R.id.levelDetails);
 
         //Level select buttons
         view.findViewById(R.id.level1Button).setOnClickListener(button -> {
-            level = 1;
-            ShowLevelDetails();
+            levelSelectOnClick(1);
         });
         view.findViewById(R.id.level2Button).setOnClickListener(button -> {
-            level = 2;
-            ShowLevelDetails();
+            levelSelectOnClick(2);
         });
         view.findViewById(R.id.level3Button).setOnClickListener(button -> {
-            level = 3;
-            ShowLevelDetails();
+            levelSelectOnClick(3);
         });
         view.findViewById(R.id.level4Button).setOnClickListener(button -> {
-            level = 4;
-            ShowLevelDetails();
+            levelSelectOnClick(4);
         });
         view.findViewById(R.id.level5Button).setOnClickListener(button -> {
-            level = 5;
-            ShowLevelDetails();
+            levelSelectOnClick(5);
         });
         view.findViewById(R.id.level6Button).setOnClickListener(button -> {
-            level = 6;
-            ShowLevelDetails();
+            levelSelectOnClick(6);
         });
         view.findViewById(R.id.level7Button).setOnClickListener(button -> {
-            level = 7;
-            ShowLevelDetails();
+            levelSelectOnClick(7);
         });
         view.findViewById(R.id.level8Button).setOnClickListener(button -> {
-            level = 8;
-            ShowLevelDetails();
+            levelSelectOnClick(8);
         });
         view.findViewById(R.id.level9Button).setOnClickListener(button -> {
-            level = 9;
-            ShowLevelDetails();
+            levelSelectOnClick(9);
         });
         view.findViewById(R.id.level10Button).setOnClickListener(button -> {
-            level = 10;
-            ShowLevelDetails();
+            levelSelectOnClick(10);
         });
         view.findViewById(R.id.level11Button).setOnClickListener(button -> {
-            level = 11;
-            ShowLevelDetails();
+            levelSelectOnClick(11);
         });
         view.findViewById(R.id.level12Button).setOnClickListener(button -> {
-            level = 12;
-            ShowLevelDetails();
+            levelSelectOnClick(12);
         });
 
         // X button
         view.findViewById(R.id.levelDetailsExitButton).setOnClickListener(button -> {
-            levelDetails.setVisibility(View.INVISIBLE);
+            slideDown(levelDetails);
         });
 
         // Start level button
@@ -100,7 +91,7 @@ public class LevelSelectFragment extends Fragment {
     }
 
     private void ShowLevelDetails() {
-        levelDetails.setVisibility(View.VISIBLE);
+//        levelDetails.setVisibility(View.VISIBLE);
 
         String[] levelDetails = LevelSelectViewModel.getLevelDetails(level);
 
@@ -122,6 +113,42 @@ public class LevelSelectFragment extends Fragment {
         Glide.with(view)
                 .load("https://image.tmdb.org/t/p/w500" + levelDetails[5])
                 .into(endImage);
+    }
+
+    // slide the view from below itself to the current position
+    public void slideUp(View view){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                view.getHeight(),  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(750);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        isUp = true;
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDown(View view){
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight()); // toYDelta
+        animate.setDuration(750);
+        animate.setFillAfter(true);
+        view.startAnimation(animate);
+        isUp = false;
+    }
+
+    private void levelSelectOnClick(int level) {
+        this.level = level;
+        ShowLevelDetails();
+        if(!isUp) {
+            slideUp(levelDetails);
+        }
+
     }
 
 }
